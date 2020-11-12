5f3b33904f53daf38ff69eda63e0cd781d6ba870
zhengshuaiwei <zhengshuaiwei@58.com>
2020-11-12 10:54:11 +0800

    提交代码工程

---
+++ NingguoForum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NingguoForum
+//
+//  Created by zhengsw on 2020/11/12.
+//  Copyright © 2020 zhengsw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'NingguoForum' do
+    pod 'SnapKit'
+    pod 'Kingfisher'
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+    pod 'HandyJSON', '5.0.0-beta'
+    pod 'ObjectMapper'
+    pod 'RxSwift'
+    pod 'RxCocoa'
+end
